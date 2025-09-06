30ebff084e4274dbe7cb6211e612d0b90a7f3e5b
Miguel Marcondes <miguelmarcondesfilho@gmail.com>
2025-09-06 17:13:18 -0300

    Add staff association and delegation methods to User model with corresponding tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  has_one :staff, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  delegate :practice, :full_name, :role, :active?, to: :staff, allow_nil: true
+
+  def staff?
+    staff.present?
+  end
 end
